4ebcbb4b1fa4e6d0a34a217c58486f3f116643d2
Ismael Requena <ismael.requena.andreu@gmail.com>
2014-04-29 15:43:52 +0200

    Transformaciones afines añadidas

--- TAfin.cpp
+++ TAfin.cpp
@@ -15,7 +15,46 @@ TAfin::TAfin(){
         m[0] = 1;
         m[5] = 1;
         m[10] = 1;
-        m[15] = 1;
+        m[15] = 1;  
+}         
+
+void TAfin::traslacion(GLfloat x, GLfloat y, GLfloat z){
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+		glLoadIdentity();
+		glTranslatef(x, y, z);
+		GLfloat* mm = new GLfloat[16];
+		glGetFloatv(GL_MODELVIEW_MATRIX,mm);
+	glPopMatrix();
+	postMultiplica(mm);		
+}
+
+void TAfin::escalacion(GLfloat x, GLfloat y, GLfloat z){
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+		glLoadIdentity();
+		glScalef(x, y, z);
+		GLfloat* mm = new GLfloat[16];
+		glGetFloatv(GL_MODELVIEW_MATRIX,mm);
+	glPopMatrix();
+	postMultiplica(mm);	
+}
+
+void TAfin::rotacion(GLfloat angle, GLfloat x, GLfloat y, GLfloat z){
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+		glLoadIdentity();
+		glRotatef(angle, x, y, z);
+		GLfloat* mm = new GLfloat[16];
+		glGetFloatv(GL_MODELVIEW_MATRIX,mm);
+	glPopMatrix();
+	postMultiplica(mm);	
+}
+
+void TAfin::postMultiplica(GLfloat matr[16]){
+	glLoadMatrixf(this->m);
+	glMultMatrixf(matr);
+        glGetFloatv(GL_MODELVIEW_MATRIX,this->m);
 }
 
 #pragma package(smart_init)
--- TAfin.h
+++ TAfin.h
@@ -12,6 +12,12 @@ class TAfin{
         public:
                 TAfin();
                 GLfloat* getMatrix(){return m;};
+				
+                void traslacion(GLfloat x, GLfloat y, GLfloat z);
+                void escalacion(GLfloat x, GLfloat y, GLfloat z);
+                void rotacion(GLfloat angle, GLfloat x, GLfloat y, GLfloat z);
+                void postMultiplica(GLfloat matr[16]);
+				
                 void setPosicion(int posicion, GLfloat numero){m[posicion] = numero;};
 };
 #endif
